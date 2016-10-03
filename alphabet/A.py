e5913a981b618999534de265ad9319ea7ccc8a7f
M Rafi <mohammad.rafi@cerner.com>
2016-10-04 02:50:53 +0530

    Add python implementation based on method_missing

---
+++ alphabet/A.py
@@ -0,0 +1,17 @@
+# cerner_2^5_2016
+# invoke methods that don't exist and beat the test!
+import sys
+from os.path import basename, splitext
+import inspect
+
+class Driver(object):
+    def __getattr__(self, method_name):
+        def _unknown(arg):
+            letter = arg 
+            if letter != 'Z':
+                self.take_test(chr(ord(arg) + 1))
+            sys.stdout.write(letter)
+        return _unknown
+
+Driver().drunk(splitext(inspect.stack()[0][1])[0])
+print
