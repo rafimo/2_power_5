33f0f1d02716908df623f438ce428b7cf49fb1de
Rafi,Mohammad <Mohammad.Rafi@Cerner.com>
2022-10-10 23:11:26 +0530

    Day 26: f-sharp: invoke a rest-api

---
+++ 2022/fsharp/Project/Program.fs
@@ -0,0 +1,26 @@
+﻿// console app in f-sharp to invoke an api and print out a response
+// get nationalities of the first arg 
+// run as below in the project directory
+//      dotnet run <name>
+// cerner_2tothe5th_2022
+// For more information see https://aka.ms/fsharp-console-apps
+open System
+open System.Net.Http
+open System.Text.Json
+
+let getData = async {
+    use httpClient = new System.Net.Http.HttpClient()
+    let args = System.Environment.GetCommandLineArgs()
+    let! response = httpClient.GetAsync($"https://api.nationalize.io/?name={args[1]}") |> Async.AwaitTask
+    response.EnsureSuccessStatusCode () |> ignore
+    let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+    printfn "Returned countries: %s" content // TODO: json deserialise this response..
+}
+
+// using an explicit entrypoint
+[<EntryPoint>]
+let main argv =
+    getData
+    |> Async.RunSynchronously // run the code!
+
+    0
